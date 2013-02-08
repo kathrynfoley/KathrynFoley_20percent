3bbcef5d42a47b7b5025c82ef33a66e5d5c98661
Kathryn Foley <kathrynfoley@hsair-119b90.westminster.net>
2013-02-08 12:53:25 -0500

    Calendar
    Project created! Started Calendar project

---
+++ Calendar.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+//Author: Kathryn Foley//
+
+class Calendar{ 
+  
+  public importEvent(String e){
+    
+    System.out.println("e");
+  }
+       
+  // The event. //
+  
+  public string event;
+  
+ 
+  // The month. //
+  
+  public string month;
+  
+  // The day of the month. //
+  
+  public int day;
+  
+  // The year. //
+  
+  public int year;
+  
+  // The time (in military time). //
+  
+  public int time;
+  
+  // Provides you with a list of your upcoming events. //
+  
+  private List<event> Events;
+  
